7d2fee6c9d0e10712a16cbfb4c474fad15ee525c
XcelevatorDharani <dharani.2530@xcelevate.org>
2025-07-30 12:17:08 +0530

    Add files via upload

---
+++ FCP005.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+public class Quotient{
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+ int A = scanner.nextInt();
+        int B = scanner.nextInt();
+int C =0;
+int D =0;
+C = A/B;
+D = A%B;
+{
+System.out.println(C);
+System.out.println(D);
+}
+}
+}
