bb6d4584a2749bad13490ef640948f4acb6225a3
XcelevatorDharani <dharani.2530@xcelevate.org>
2025-07-31 12:39:00 +0530

    Add files via upload

---
+++ FCP024.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Even{
+public static  void main(String[]args){
+Scanner scanner = new Scanner(System.in);
+
+int N = scanner.nextInt();
+for(int i=2;i<N;i++){
+
+if(i % 2 == 0){
+System.out.println(i);
+}
+}
+
+}
+}
+
