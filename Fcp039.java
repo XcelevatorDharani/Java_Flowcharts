705edbd717e62057ea44beaf1e562384dead25ae
XcelevatorDharani <dharani.2530@xcelevate.org>
2025-10-02 10:32:38 +0530

    Update Fcp039.java

--- Fcp039.java
+++ Fcp039.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
-public class FCP039{
-	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int a = sc.nextInt();
-		int max = a;
-		int min = a;
+public class FCP039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-		for (int i=1 ; i<n; i++){
-			int b = sc.nextInt();
-			if (b>max){
-				max=b;
-			}
-			else{
-				min=b;
-			}
-		}
-		System.out.println(max);
-		System.out.println(min);
-	}
+        int a = sc.nextInt();
+        int max = a;
+        int min = a;
+
+        for (int i = 1; i < n; i++) {
+            int b = sc.nextInt();
+            if (b > max) {
+                max = b;       
+            }
+            if (b < min) {
+                min = b;       
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+    }
 }
 
-		
